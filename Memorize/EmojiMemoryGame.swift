19e147edcd73a621ecdc9d0ea7f6d71f19c6d62d
OSrB2 <pedro_oliva@outlook.com.br>
2025-05-16 15:56:20 -0300

    feat: create funcs

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,28 @@
 //  Created by Pedro Oliveira on 16/05/25.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+	@Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemomoryGame()
+	
+	static func createMemomoryGame() -> MemoryGame<String> {
+		let emojis = ["👻", "🎃", "🕷️"]
+		return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { parIndex in
+			return emojis[parIndex]
+		}
+	}
+	
+	
+	// MARK: - Access to the model
+	
+	var cards: Array<MemoryGame<String>.Card> {
+		model.cards
+	}
+	
+	// MARK: - Intent(s)
+	
+	func choose(card: MemoryGame<String>.Card) {
+		model.choose(card: card)
+	}
+}
